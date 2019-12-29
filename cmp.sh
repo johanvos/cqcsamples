49d4daca397d60bbd4a048e6dfaba6ca5ece6544
Johan Vos <johan@lodgon.com>
2019-12-29 21:32:35 +0100

    first sample

---
+++ cmp.sh
@@ -0,0 +1,1 @@
+javac -d class -cp ~/quantumcomputing/work/fork/strange/build/libs/strange-0.0.11-SNAPSHOT.jar:. src/Teleport.java
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp ~/quantumcomputing/work/fork/strange/build/libs/strange-0.0.11-SNAPSHOT.jar:class Teleport
